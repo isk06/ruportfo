992e57f7c3be0161077ebafac1ed115695ffac7f
Iskander-Daraev <2364139@gmail.com>
2025-12-26 17:34:56 +0300

    social

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,7 @@ export default function HomePage() {
       </a>
 
       <a
-        href="https://www.behance.net/iskdaraev"
+        href="https://www.google.com"
         target="_blank"
         rel="noreferrer"
       >
