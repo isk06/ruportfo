69ab0d619279c6bd3cb70914f7e81cb0811332ea
Iskander-Daraev <2364139@gmail.com>
2025-12-31 13:52:40 +0300

    description

--- app/page.tsx
+++ app/page.tsx
@@ -85,7 +85,7 @@ export default function HomePage() {
         rel="noreferrer"
       >
         <img
-          src="/icons/mail4.svg"
+          src="/icons/mail5.svg"
           alt="E-mail"
           style={{ width: 230, height: 230 }}
         />
