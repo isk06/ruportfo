9dd7a096076326cbe01117132fb75d32de9ece84
Iskander-Daraev <2364139@gmail.com>
2026-01-13 22:10:02 +0300

    del fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,24 @@
 import "./globals.css";
+import { Manrope } from "next/font/google";
+
+const manrope = Manrope({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  display: "swap",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={manrope.className}>{children}</body>
+    </html>
+  );
+}
+
 
 export const metadata = {
   title: "Iskander Daraev - portfolio",
@@ -9,14 +29,4 @@ export const metadata = {
   },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
+
